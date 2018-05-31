7d27a96cc9ac8a14c5ed374d3525750f95e54751
Talzoor <talzoorjunk@gmail.com>
2018-05-31 16:26:34 +0300

    upload

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 # main
 import serial
+import time
 
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 ser.close()
 ser.open()
 
 data = "test!"
-ser.write(data.encode())
+
 
 try:
     while 1:
-        response = ser.readline()
-        print(response)
+        ser.write(data.encode())
+        print('sent')
+        time.sleep(1)
+        if ser.inWaiting():
+            response = ser.readline()
+            print(response)
 
 except KeyboardInterrupt:
     ser.close()
