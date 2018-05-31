8ab0c01770c94e75d2954532f048bc9c29fe0c08
Talzoor <talzoorjunk@gmail.com>
2018-05-31 15:30:44 +0300

    upload

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # main
+import serial
+
+ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+ser.open()
+
+ser.write("test!")
+
+try:
+    while 1:
+        response = ser.readline()
+        print (response)
+
+except KeyboardInterrupt:
+    ser.close()
