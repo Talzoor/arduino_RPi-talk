1845d96db892537ad1c6915c816081da510d1c35
Talzoor <talzoorjunk@gmail.com>
2018-05-31 16:18:11 +0300

    upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import serial
 
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+ser.close()
 ser.open()
 
 ser.write("test!")
@@ -9,7 +10,7 @@ ser.write("test!")
 try:
     while 1:
         response = ser.readline()
-        print (response)
+        print(response)
 
 except KeyboardInterrupt:
     ser.close()
